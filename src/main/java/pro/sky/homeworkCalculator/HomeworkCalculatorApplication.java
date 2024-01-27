1d7da2d6db1d035820fcca71a75f85cdadc04566
Zing27sky <Zing27@mail.ru>
2024-01-28 02:07:31 +0300

    initial commit

---
+++ src/main/java/pro/sky/homeworkCalculator/HomeworkCalculatorApplication.java
@@ -0,0 +1,13 @@
+package pro.sky.homeworkCalculator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HomeworkCalculatorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HomeworkCalculatorApplication.class, args);
+    }
+
+}
